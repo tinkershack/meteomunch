3e4f8cb8ff12ae8fc946622f9867e0a2b704a500
Karthik G <23f3000528@es.study.iitm.ac.in>
2024-10-13 10:56:17 +0530

    Bug fix: set config defaults  (#8)
    * Added validation for critical config feilds and set defaults for missing non critical feilds
    
    * Update server routes in server.go based on selected weather provider
    
    - Adjusted base URL and query parameters for open-meteo and meteoblue.
    - Improved routing logic for fetching weather data.
    
    * Addressed feedback: improved validation and handled defaults for missing config fields
    
    * feat Added changes for config.go
    
    - removed the document store
    - initially made the conf to the default conf
    - removed the unwanted logic for setting defaults
    - made the validateCriticalFeilds more clearer
    
    * Added the Issue templates

---
+++ .github/ISSUE_TEMPLATE/feature_request.md
@@ -0,0 +1,33 @@
+# Feature Request
+
+## Summary
+
+Provide a brief description of the feature you are requesting.
+
+## Motivation
+
+Explain why this feature is necessary and what problem it will solve.
+
+## Detailed Design
+
+Describe the proposed design and implementation details.
+
+## Alternatives
+
+List any alternative solutions or features you've considered.
+
+## Additional Context
+
+Add any other context about the feature request here.
+
+## Acceptance Criteria
+
+Define the criteria that must be met for the feature to be considered complete.
+
+- [ ] Criterion 1
+- [ ] Criterion 2
+- [ ] Criterion 3
+
+## Additional Information
+
+Include links to any relevant resources, documentation, or related issues.
--- config/config.go
+++ config/config.go
@@ -5,14 +5,14 @@ package config
 
 import (
 	"log/slog" // avoiding logger package to prevent cyclic imports
-
+	"os"
+	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	e "github.com/tinkershack/meteomunch/errors"
 )
 
 type config struct {
 	Munch          Munch     // Parameters of munch app, excluding external dependencies
-	DocumentStore  string    // Name of the preferred DS
 	Mongo          DataStore // Gets picked if DocumentStore is "mongo"
 	DLMRedis       DataStore
 	MeteoProviders []MeteoProvider
@@ -20,10 +20,6 @@ type config struct {
 
 func (c *config) GetMunch() Munch {
 	return c.Munch
-}
-
-func (c *config) GetDocumentStore() string {
-	return c.DocumentStore
 }
 
 func (c *config) GetMongo() DataStore {
@@ -38,7 +34,7 @@ func (c *config) GetMeteoProviders() []MeteoProvider {
 	return c.MeteoProviders
 }
 
-// TODO: Validate URL sting
+// TODO: Validate URL string
 type MeteoProvider struct {
 	Name    string
 	APIKey  string
@@ -63,21 +59,75 @@ type DataStore struct {
 	DBNumber int
 }
 
-var legal = &struct {
-	documentStore []string
-}{
-	documentStore: []string{"mongo"},
+var defaultConfig = &config{
+	Munch: Munch{
+		Server: MunchServer{
+			Hostname: "localhost",
+			Port:     "50050",
+		},
+		LogLevel: "info",
+	},
+	Mongo: DataStore{
+		Name:     "mongo",
+		URI:      "mongodb://localhost:27017",
+		DBName:   "meteomunch",
+		DBNumber: 0,
+	},
+	DLMRedis: DataStore{
+		Name:     "redis",
+		URI:      "redis://localhost:6379",
+		DBNumber: 1,
+	},
+	MeteoProviders: []MeteoProvider{
+		{
+			Name:    "open-meteo",
+			APIKey:  "",
+			APIPath: "v1/forecast",
+			BaseURI: "https://api.open-meteo.com/",
+		},
+	},
 }
 
-// TODO: This can be more perfomant by loading it just once and pass a copy for each call
-func New() (*config, error) {
-	// log := logger.NewTag("config")
-	conf := new(config)
 
-	if err := viper.Unmarshal(conf); err != nil {
-		slog.Error(e.FAIL, "err", err, "description", "Couldn't parse config")
-		return nil, err
+// New creates a new config object
+func New() (*config, error) {
+	conf := defaultConfig
+
+	pwd, err := os.Getwd()
+	cobra.CheckErr(err)
+	viper.AddConfigPath(pwd)
+	viper.AddConfigPath(".")
+	viper.SetConfigType("yml")
+	viper.SetConfigName("munch")
+
+	if err := viper.ReadInConfig(); err != nil {
+		slog.Warn("Config file not found, using default config", "error", err)
+		return defaultConfig, nil 
 	}
 
+	if err := viper.Unmarshal(conf); err != nil {
+		slog.Error(e.FAIL, "err", err, "description", "Couldn't parse config, using default config")
+		return defaultConfig, nil 
+	}
+	// Check for critical values and panic if missing
+	validateCriticalFields(conf)
+
 	return conf, nil
+}
+// validateCriticalFields checks for critical parameters in the configuration
+func validateCriticalFields(conf *config) {
+	for _, provider := range conf.MeteoProviders {
+		if provider.Name == "meteoblue" && provider.APIKey == "" {
+			slog.Error(e.FAIL, "error", "Critical config value missing: APIKey for MeteoProvider", "provider", provider.Name)
+			panic("Critical config value is missing: API key for the provider")
+		}
+		if(provider.Name=="meteoblue" && provider.APIPath==""){
+			slog.Error(e.FAIL,"error","Critical config value missing : API path for the meteoprovider","provider",provider.Name)
+			panic("Critical config value is missing: APi path for the provider")
+		}
+		if(provider.Name=="meteoblue" && provider.BaseURI==""){
+			slog.Error(e.FAIL,"error","Critical config vlaue is missing: BaseURI for the meteoprovider","provider",provider.Name)
+			panic("Critical config value is missing : BaseURI for the provider")
+		}
+	}
 }
